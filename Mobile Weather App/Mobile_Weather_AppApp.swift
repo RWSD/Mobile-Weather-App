21c22e54fb0d70439b19e15530b004fece465f70
RWSD <61355397+RWSD@users.noreply.github.com>
2021-02-23 10:42:43 +0100

    Initial Commit

---
+++ Mobile Weather App/Mobile_Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mobile_Weather_AppApp.swift
+//  Mobile Weather App
+//
+//  Created by Reinhard Wunderer on 23.02.21.
+//
+
+import SwiftUI
+
+@main
+struct Mobile_Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
